fd6d9ee4c2792e436a663247684ffa291bcfcd9a
Matt Sasso <mjs2430@gmail.com>
2023-12-22 14:06:00 -0500

    added lists

--- app/src/main/java/com/example/journal/NoteAdapter.java
+++ app/src/main/java/com/example/journal/NoteAdapter.java
@@ -29,12 +29,22 @@ public class NoteAdapter extends ArrayAdapter<Note> {
         timestampTextView.setText(note.getTimestamp());
         noteTextView.setText(note.getText());
 
-        // replace '-' with bullets
-        String noteText = note.getText()
-                .replace(" -", "\t\u2023")  // replace " -" with tab and bullet
-                .replace("-", "\u2022");  // replace "-" with bullet
+// replace '-' with bullets and add bullet if new line starts with space
+        String[] lines = note.getText().split("\n");
+        StringBuilder noteText = new StringBuilder();
 
-        noteTextView.setText(noteText);
+        for (String line : lines) {
+            if (line.startsWith("  ")) {
+                noteText.append("\n\t\t\u25cb ").append(line.substring(2));
+            } else if (line.startsWith(" ")) {
+                noteText.append("\n\t\u25cf ").append(line.substring(1));
+            } else {
+                line = line.replace(" -", "\n\t\u2023"); // replace " -" with tab and bullet
+                line = line.replace("-", "\u2022"); // replace "-" with bullet
+                noteText.append("\n").append(line);
+            }
+        }
+        noteTextView.setText(noteText.toString().trim()); // trim() to remove the leading '\n'
 
         return convertView;
     }
