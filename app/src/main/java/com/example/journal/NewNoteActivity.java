eb7e8b576ba52b1f64a1536dbe7803059dfba5a1
Matt Sasso <mjs2430@gmail.com>
2023-12-22 15:30:46 -0500

    cam button

--- app/src/main/java/com/example/journal/NewNoteActivity.java
+++ app/src/main/java/com/example/journal/NewNoteActivity.java
@@ -1,14 +1,47 @@
 package com.example.journal;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewNoteActivity extends AppCompatActivity {
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 100:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1888);
+                } else {
+                    Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+    public void openCamera(View view) {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        } else {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, 1888);
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1888 && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            // ImageView imageView = (ImageView) this.findViewById(R.id.imageView1);
+            // imageView.setImageBitmap(photo);
+            // Do something with photo
+        }
+    }
     private EditText noteEditText;
 
     @Override
